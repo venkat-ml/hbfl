51fd9dded8583a26c5c6bd97755a729b3ad584a3
Venkat Lanka <Venkat.Lanka@transport.nsw.gov.au>
2021-05-11 08:40:01 +1000

    Module-4 - Completed
    - S3 storage and uploading objects

--- scripts/module_04/upload-s3-objects.js
+++ scripts/module_04/upload-s3-objects.js
@@ -2,11 +2,11 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'ap-southeast-2' })
 
 // Declare local variables
 const s3 = new AWS.S3()
-const bucketName = '/* TODO: Make this bucket name match what you created */'
+const bucketName = 'hamster-bucket-dplvs21'
 
 helpers.getPublicFiles()
 .then(files => uploadS3Objects(bucketName, files))
@@ -14,14 +14,25 @@ helpers.getPublicFiles()
 
 function uploadS3Objects (bucketName, files) {
   // TODO: Define putObject params object
+  const params = {
+    Bucket : bucketName,
+    ACL : 'public-read'
+  }
 
   const filePromises = files.map((file) => {
     const newParams = Object.assign({}, params, {
       // TODO: Add individual file params
+      Body : file.contents,
+      Key : file.name,
+      ContentType : helpers.getContentType(file.name)
     })
 
     return new Promise((resolve, reject) => {
       // TODO: Put objects in S3
+      s3.putObject(newParams, (err, data) => {
+        if (err) reject (err)
+        else resolve(data)
+      })
     })
   })
 
