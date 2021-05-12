ea96d32fff75e5a82e560741d2d851df75bcfdd8
Venkat Lanka <Venkat.Lanka@transport.nsw.gov.au>
2021-05-12 13:56:35 +1000

    Module-7 - Completed
    - CloudFront and S3 website hosting

--- scripts/module_07/cloudfront-parameters.js
+++ scripts/module_07/cloudfront-parameters.js
@@ -1,11 +1,48 @@
-function origins (bucketName) {
+function origins(bucketName) {
   return {
     // TODO: Add properties for Origins
+    Quantity: 1,
+    Items: [
+      {
+        DomainName: '${bucketName}.s3.amazonaws.com',
+        Id: '${bucketName}_origin',
+        S3OriginConfig: {
+          OriginAccessIdentity: ''
+        }
+      }
+    ]
   }
 }
-function defaultCacheBehavior (bucketName) {
+function defaultCacheBehavior(bucketName) {
   return {
     // TODO: Add properties for DefaultCacheBehavior
+    ForwardedValues: {
+      Cookies: {
+        Forward: 'none'
+      },
+      QueryString: false
+    },
+    MinTTL: 0,
+    TargetOriginId: '${bucketName}_origin',
+    TrustedSigners: {
+      Quantity: 0,
+      Enabled: false
+    },
+    ViewerProtocolPolicy: 'redirect-to-https',
+    AllowedMethods: {
+      Quatity: 2,
+      Items: [
+        'GET',
+        'HEAD'
+      ],
+      CacheMethods: {
+        Quantity: 2,
+        Items: [
+          'GET',
+          'HEAD'
+        ]
+      }
+    }
   }
 }
 
--- scripts/module_07/create-cloudfront-distribution.js
+++ scripts/module_07/create-cloudfront-distribution.js
@@ -2,18 +2,35 @@
 const AWS = require('aws-sdk')
 const cfParams = require('./cloudfront-parameters')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'ap-southeast-2' })
 
 // Declare local variables
 // TODO: Create CloudFront SDK Object
+const cf = new AWS.CloudFront()
 
-createDistribution('/* TODO: Add your bucket name */')
+createDistribution('hamster-bucket-dplvs21')
 .then(data => console.log(data))
 
 function createDistribution (bucketName) {
   // TODO: Create params const object
+  const params = {
+    DistributionConfig : {
+      CallerReference : '${Date.now()}',
+      Comment : 'HBFL Distribution',
+      DefaultCacheBehaviour : cfParams.defaultCacheBehavior(bucketName),
+      Origins : cfParams.origins(bucketName),
+      HttpVersion : 'http2',
+      PriceClass : 'PriceClass-100',
+      IsIPV6Enabled : true,
+      Enabled : true
+    }
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Call createDistribution
+    cf.createDistribution(params, (err, data) => {
+      if (err) reject (err)
+      else resolve(data)
+    })
   })
 }
